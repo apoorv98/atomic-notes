cccd582b7ce2a116bdd88666014b9760955673d7
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2024-07-19 10:47:45 +0530

    tests for initial note conversion logic

---
+++ backend/tests/test_note_conversion.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import pytest
+from app.services.note_conversion_service import convert_to_atomic_notes
+
+def test_convert_to_atomic_notes():
+    content = """# Main Topic
+
+## Subtopic 1
+This is some content for subtopic 1.
+
+## Subtopic 2
+This is some content for subtopic 2.
+
+### Sub-subtopic 2.1
+More detailed information here.
+"""
+
+    atomic_notes = convert_to_atomic_notes(content)
+    assert len(atomic_notes) == 4
+    assert atomic_notes[0]['title'] == 'Main Topic'
+    assert atomic_notes[1]['title'] == 'Subtopic 1'
+    assert atomic_notes[2]['title'] == 'Subtopic 2'
+    assert atomic_notes[3]['title'] == 'Sub-subtopic 2.1'
+
+def test_convert_to_atomic_notes_with_backlinks():
+    content = """# Main Topic
+
+## Subtopic 1
+This is some content for subtopic 1.
+See also: [[Subtopic 2]]
+
+## Subtopic 2
+This is some content for subtopic 2.
+Related to [[Subtopic 1]]
+"""
+
+    atomic_notes = convert_to_atomic_notes(content)
+    assert len(atomic_notes) == 3
+    assert '[[Subtopic 2]]' in atomic_notes[1]['content']
+    assert '[[Subtopic 1]]' in atomic_notes[2]['content']
