37f2c3652392ed45c9d9d567864ac682ade7ee6b
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2024-07-19 10:51:50 +0530

    create note model

---
+++ backend/app/models/note.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from app import db
+from datetime import datetime
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    user = db.relationship('User', backref=db.backref('notes', lazy=True))
